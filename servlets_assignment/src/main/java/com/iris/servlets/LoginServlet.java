532dbac775bc6439e0d43c28f8b9560b83cee401
Isimbi Iris <irisisimbi@gmail.com>
2026-02-01 15:38:09 +0200

    Assignment 1: Login Servlet completed

---
+++ servlets_assignment/src/main/java/com/iris/servlets/LoginServlet.java
@@ -0,0 +1,31 @@
+
+package com.iris.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if (password.length() < 8) {
+            out.println("<h3>Hello " + username + ", your password is weak.</h3>");
+        } else {
+            out.println("<h3>Welcome " + username + "</h3>");
+        }
+    }
+}
